8aa60b991f626f05d56545ff2b87dd013f1b23ba
olivia.mo <olivia.mo@carsguide.com.au>
2018-03-13 08:36:24 +1100

    Added true/false state to stop the game after a winner is declared

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
-var scores, roundScore, activePlayer;
+// declaring variables in global scope so all functions will have access to these
+var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
 // addEventListener registers a single event listener on a single target. 2 args: 1. the event type e.g. click. 2. the function to be called when the event happens. note: you can use anonymous function as argument instead of external function like btn() on line 6 e.g. function() {}
 function btn() {
-  // 1. random number
-  // Math.floor removes the decimals
-  // Math.random gives a random number between 0 and 1. We multiply that by 6 to get random number between 0 - 5. We add 1 to get random number between 1 - 6.
-  var dice = Math.floor(Math.random() * 6) + 1;
+  if (gamePlaying) {
+    // 1. random number
+    // Math.floor removes the decimals
+    // Math.random gives a random number between 0 and 1. We multiply that by 6 to get random number between 0 - 5. We add 1 to get random number between 1 - 6.
+    var dice = Math.floor(Math.random() * 6) + 1;
 
-  //2. display result: unhiding the dice after 'rolling' it
-  var diceDOM = document.querySelector('.dice');
-  diceDOM.style.display = 'block';
-  // changing the dice image src
-  diceDOM.src = 'dice-' + dice + '.png';
+    //2. display result: unhiding the dice after 'rolling' it
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    // changing the dice image src
+    diceDOM.src = 'dice-' + dice + '.png';
 
-  // 3. update the round score IF the rolled number is NOT a 1
-  if (dice !== 1) {
-    // Add score
-    roundScore += dice; // same as roundScore = roundScore + dice;
-    document.querySelector('#current-' + activePlayer).textContent = roundScore; // displaying the round score
-  } else {
-    // next player
-    nextPlayer();
+    // 3. update the round score IF the rolled number is NOT a 1
+    if (dice !== 1) {
+      // Add score
+      roundScore += dice; // same as roundScore = roundScore + dice;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore; // displaying the round score
+    } else {
+      // next player
+      nextPlayer();
 
-    document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.dice').style.display = 'none';
+    }
   }
+
 }
 document.querySelector('.btn-roll').addEventListener('click', btn);
 // 2nd arg (function) we don't need to add () because we're not calling it, the event listener will call the function for us. This is known as callback function; a function that is not called by us, but by another function. The function that is pass into another function as an argument and this function (addEventListener method) will then call that function for us
@@ -35,25 +39,28 @@ document.querySelector('.btn-roll').addEventListener('click', btn);
 
 // button hold - hold the score and change active player
 document.querySelector('.btn-hold').addEventListener('click', function() { // anonymous function
-  // Add current score to global score
-  // score[0] is player0; score[1] is player1. We're adding activePlayer's round score to their global score
-  scores[activePlayer] += roundScore;
+  if (gamePlaying) {
+    // Add current score to global score
+    // score[0] is player0; score[1] is player1. We're adding activePlayer's round score to their global score
+    scores[activePlayer] += roundScore;
 
-  // Update the UI
-  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    // Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
 
-  // Check if player won the game
-  if (scores[activePlayer] >= 100) {
-    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-    document.querySelector('.dice').style.display = 'none';
-    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-  } else {
-    // Next player if there's no winner
-    nextPlayer();
+    // Check if player won the game
+    if (scores[activePlayer] >= 20) {
+      document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+      document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+      gamePlaying = false;
+    } else {
+      // Next player if there's no winner
+      nextPlayer();
+    }
   }
-
+  
 });
 
 
@@ -83,6 +90,7 @@ function init() {
   scores = [0,0];
   roundScore = 0;
   activePlayer = 0; // 0 is first player, 1 is second player
+  gamePlaying = true;
 
   // we want to change the style to display:none because when the user first opens the webpage, the dice should not be visible
   document.querySelector('.dice').style.display = 'none';
