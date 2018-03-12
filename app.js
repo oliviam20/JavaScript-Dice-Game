b23a6d32d92161700d309e11262c027b45c1f97f
olivia.mo <olivia.mo@carsguide.com.au>
2018-03-12 20:32:28 +1100

    hid dice image upon page load

--- app.js
+++ app.js
@@ -12,5 +12,15 @@ console.log(dice);
 
 // querySelector lets us select stuff exactly the way we do it in CSS. The only difference is that it selects the first element it finds
 // like CSS to select ids, we use #
-// textContent: gets or sets the text content of a node and its descendants
+// textContent: gets or sets the text content of a node and its descendants. textContent can only set plain text, no HTML
 document.querySelector('#current-0').textContent = dice;
+
+// innerHTML lets us add html elements. (this is example of above line)
+// document.querySelector('#current-0').innerHTML = '<em>' + dice + '</em>';
+
+// getter: we get a value
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+// we want to change the style to display:none because when the user first opens the webpage, the dice should not be visible
+document.querySelector('.dice').style.display = 'none';
