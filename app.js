6eba997093aa41e526b51745a82cfa791221cd15
olivia.mo <olivia.mo@carsguide.com.au>
2018-03-13 09:24:07 +1100

    Changed winning condition back to 100 from testing

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() { // an
 
 
     // Check if player won the game
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
       document.querySelector('.dice').style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
@@ -60,7 +60,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() { // an
       nextPlayer();
     }
   }
-  
+
 });
 
 
